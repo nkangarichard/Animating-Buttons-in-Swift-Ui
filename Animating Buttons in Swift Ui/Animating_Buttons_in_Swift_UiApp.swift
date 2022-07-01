6d50d12c851681c73595b0cb70f268a8870df618
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2022-07-01 09:55:14 +0100

    Initial Commit

---
+++ Animating Buttons in Swift Ui/Animating_Buttons_in_Swift_UiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_Buttons_in_Swift_UiApp.swift
+//  Animating Buttons in Swift Ui
+//
+//  Created by Richard Nkanga on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Animating_Buttons_in_Swift_UiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animating Buttons in Swift Ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animating Buttons in Swift Ui
+//
+//  Created by Richard Nkanga on 01/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
